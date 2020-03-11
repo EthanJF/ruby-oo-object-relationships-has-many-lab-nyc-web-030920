fc3b021f31c344919168e1e623c8e938d26f448a
Ethan Fournier <ethanjosephfournier@gmail.com>
2020-03-11 14:04:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,14 +1,19 @@
 class Artist
   
-  attr_accessor :name, :songs
+  attr_accessor :name
   
   def initialize(name)
     @name = name
     @songs = []
   end
   
+  def songs
+    @songs
+  end
+  
   def add_song(title)
     @songs << title
+    song.artist = self
   end
     
   
